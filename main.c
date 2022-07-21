f1385e1c00400bca27d67b5d1feebb7c69bf57e9
Mathis <82944306+M3tex@users.noreply.github.com>
2022-07-21 17:43:28 +0200

    Small Fixes

--- main.c
+++ main.c
@@ -7,9 +7,8 @@ int main(void)
 {
     clear_screen();
     enable_raw_mode();
+    init_terminal();
 
-
-    // printf("\033[30;107mHello\033[0m"); // Fond blanc
     while (1)
     {
         update_terminal();
